54f8554e8c332875e518ddeab2712897c2ff1504
Adrian Wong <adrianwjw@gmail.com>
2018-05-04 19:46:00 +1000

    fix: Use 'Current' symlink instead of explicit Ruby version

--- install.sh
+++ install.sh
@@ -28,6 +28,6 @@ create_symlink '.vim'
 create_symlink '.vimrc'
 
 pushd '.vim/bundle/command-t/ruby/command-t/ext/command-t'
-/System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/bin/ruby extconf.rb
+/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby extconf.rb
 make
 popd
